de55b256fb14fbbdd3f3a75339711302d27c70f0
Glitch (nonstop-exclusive-amaranthus) <none>
2020-10-06 11:53:01 +0000

    👂🎇 Checkpoint ./astronaute.py:22204162/509 ./requirements.txt:22204162/10 ./start.sh:22204162/1

--- astronaute.py
+++ astronaute.py
@@ -4,17 +4,21 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='!')
 bot.remove_command('help')
 
-for file in os.listdir("events"):
-    if file.endswith(".py"):
-        name = file[:-3]
-        bot.load_extension(f"events.{name}")
+def run():
+    bot.run("NzU3MTg3NTQ3NTQ0NjgyNTk3.X2cv2w.f08QQwi-B-dS6IEM6RUGsrcJW10")
 
-for file in os.listdir("cogs"):
-    if file.endswith(".py"):
-        name = file[:-3]
-        bot.load_extension(f"cogs.{name}")
+    
+@bot.event
+async def on_ready():
+    for file in os.listdir("events"):
+        if file.endswith(".py"):
+            name = file[:-3]
+            bot.load_extension(f"events.{name}")
+
+    for file in os.listdir("cogs"):
+        if file.endswith(".py"):
+            name = file[:-3]
+            bot.load_extension(f"cogs.{name}")
 
 print("Serveur ON")
-
-if __name__ == "__main__":
-    bot.run("NzU3MTg3NTQ3NTQ0NjgyNTk3.X2cv2w.f08QQwi-B-dS6IEM6RUGsrcJW10")
+    
