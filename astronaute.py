a752c1e214da2c921dbf81e11c4ba7cd987f76be
Glitch (nonstop-exclusive-amaranthus) <none>
2020-10-06 11:40:53 +0000

    🏕🙎 Checkpoint ./astronaute.py:22204162/260 ./website.py:22204162/517 ./requirements.txt:22204162/10 ./start.sh:22204162/1

--- astronaute.py
+++ astronaute.py
@@ -4,8 +4,6 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='!')
 bot.remove_command('help')
 
-
-
 for file in os.listdir("events"):
     if file.endswith(".py"):
         name = file[:-3]
@@ -16,12 +14,7 @@ for file in os.listdir("cogs"):
         name = file[:-3]
         bot.load_extension(f"cogs.{name}")
 
-@app.route('/')
-def homepage():
-    return render_template('index.html')
-
 print("Serveur ON")
 
 if __name__ == "__main__":
-    app.run()
     bot.run("NzU3MTg3NTQ3NTQ0NjgyNTk3.X2cv2w.f08QQwi-B-dS6IEM6RUGsrcJW10")
--- website.py
+++ website.py
@@ -1,3 +1,11 @@
 from flask import Flask, render_template
+from threading import Thread
+import astronaute
+
+t = Thread(target=bot.run)
+t.start()
 
 app = Flask(__name__, static_folder='public', template_folder='views', static_url_path='')
+@app.route('/')
+def homepage():
+    return render_template('index.html')
