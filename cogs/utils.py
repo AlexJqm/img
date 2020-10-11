288ca7ef92533c9d526150e0eb3a663806054016
Glitch (nonstop-exclusive-amaranthus) <none>
2020-10-11 02:08:12 +0000

    🌅😋 Checkpoint ./cogs/utils.py:22204162/639

--- cogs/utils.py
+++ cogs/utils.py
@@ -106,27 +106,28 @@ class Utils(commands.Cog):
             await ctx.channel.purge()
 
     @commands.command(pass_context = True)
-    async def rules(ctx):
+    async def rules(self, ctx):
         await ctx.channel.purge(limit = 1)
         role = [role.name for role in ctx.message.author.roles if role.name == "Admin" or role.name == "Modérateur" or role.name == "Modérateur+"]
         if 'Admin' in role or 'Modérateur' in role or 'Modérateur+' in role:
-            embed = discord.Embed(title = "Les règles", description = "Le serveur Discord de Among Us Francophone est un lieu de vie commune où vous rencontrerez des joueurs / joueuses de tout âge et de tout horizon. Il est donc impératif de bien lire ce règlement pour éviter tout débordement. Nous sommes particulièrement attentifs et vigilants aux contenus proposés par les joueurs dans les  channels textuels. Nous vous recommandons donc de faire attention a vos propos.", color = 0xf7f7f7)
-            embed.add_field(name = "**0.** Nous suivons les termes d'utilisation de discord", value = "• Charte d’Utilisation de la Communauté:\nhttps://discordapp.com/guidelines\n• Conditions d'Utilisation:\nhttps://discordapp.com/terms", inline = False)
+            embed = discord.Embed(title = "Les règles", description = "Le serveur Discord de Among Us Francophone est un lieu de vie commune où vous rencontrerez des joueurs / joueuses de tout âge et de tout horizon. Il est donc impératif de bien lire ce règlement pour éviter tout débordement. Nous sommes particulièrement attentifs et vigilants aux contenus proposés par les joueurs dans les channels textuels et émis dans les channels vocaux. Nous vous recommandons donc de faire attention a vos propos.", color = 0xf7f7f7)
+            embed.add_field(name = "**0.** Nous suivons les termes d'utilisation de Discord", value = "• Charte d’Utilisation de la Communauté:\nhttps://discordapp.com/guidelines\n• Conditions d'Utilisation:\nhttps://discordapp.com/terms", inline = False)
             embed.add_field(name = "\u200B", value = "**1.** Soyez tout simplement respectueux et gentil les uns envers les autres, c'est un jeu. Restez polis et courtois. Les formules de politesse telles que 'Bonjour/Au revoir/Merci/S'il te plait' n'ont jamais tué personne.", inline = False)
             embed.add_field(name = "\u200B", value = "**2.** Il est strictement interdit de grief (troll/anti-jeu), par exemple si vous mourrez, vous ne parlez pas.", inline = False)
             embed.add_field(name = "\u200B", value = "**3.** Utilisez un pseudo correct , non insultant ou provocant. Vous pouvez signalez tout pseudo qui vous paraît ne pas être conforme au règlement. La modération se réserve le droit de supprimer votre pseudo. Même règlement pour les avatars.", inline = False)
             embed.add_field(name = "\u200B", value = "**4.** Le spam sur le discord est bien évidemment interdit ainsi que toute sorte de harcèlement ou de jugement raciste et/ou faisant la promotion d'une quelconque sorte de haine.", inline = False)
             embed.add_field(name = "\u200B", value = "**5.** Aucun contenu NSFW n'est autorisé (Not Safe For Work) autrement dit pornographique ou choquant.", inline = False)
-            embed.add_field(name = "**6.** Quand vous êtes en vocal vous **DEVEZ**:", value = "**6.1** - Être mute jusqu'à tant qu'un 'meeting' commence.\n**6.2** - Mute dès que le vote est terminé. Pas après.\n**6.3** - Ne plus parler dès que vous êtes mort.\n**6.4** - Ne restez pas AFK.\n**6.5** - Avoir un micro acceptable.\n**6.6** - Ses règles sont générales, vous pouvez créer vos propres règles si tout le monde et d'accord avec avant le début de la partie. Nous vous connectons, mais ne créons pas de règles.", inline = False)
+            embed.add_field(name = "\u200B", value = "**6.** Quand vous êtes en vocal vous **DEVEZ**:\n**6.1** - Être mute jusqu'à tant qu'un 'meeting' commence.\n**6.2** - Mute dès que le vote est terminé. Pas après.\n**6.3** - Ne plus parler dès que vous êtes mort.\n**6.4** - Ne restez pas AFK.\n**6.5** - Avoir un micro acceptable.\n**6.6** - Ses règles sont générales, vous pouvez créer vos propres règles si tout le monde et d'accord avec avant le début de la partie. Nous vous connectons, mais ne créons pas de règles.", inline = False)
             embed.add_field(name = "\u200B", value = "**7.** Veillez à utiliser les channels approprié a vos demandes.", inline = False)
             embed.add_field(name = "\u200B", value = "**8.** Toute insulte ou divulgation d'informations personnelles est strictement interdite.", inline = False)
-            embed.add_field(name = "\u200B", value = "**9.** Merci de ne pas explicitivement montrer publiquement vos opinions politiques, religieuses ou tout autre pouvant créer à un débat stérile..", inline = False)
-            embed.add_field(name = "\u200B", value = "**10** En cas de problèmes, spams MP/DM, insultes, menaces, caractères raciste et 'j'en-passe', contacté l'un des modérateurs disponible avec le channel #report sans oubliez de mentionner le joueur et le channel qui nécessite une intervention et écrivez un signalement avec si possible avec des preuves.", inline = False)
+            embed.add_field(name = "\u200B", value = "**9.** Merci de ne pas explicitivement montrer publiquement vos opinions politiques, religieuses ou tout autre pouvant créer à un débat stérile.", inline = False)
+            embed.add_field(name = "\u200B", value = "**10.** En cas de problèmes, spams MP/DM, insultes, menaces, caractères raciste et 'j'en-passe', contacté l'un des modérateurs disponible avec le channel #report sans oubliez de mentionner le joueur et le channel qui nécessite une intervention et écrivez un signalement avec si possible avec des preuves.", inline = False)
             embed.add_field(name = "\u200B", value = "Si vous êtes pas content d’une sanction ou warn , nous sommes aptes à discutez avec vous . Dans le respect et faire une étude de cas.", inline = False)
-            embed.add_field(name = "\u200B", value = "Sachez que le staff communique via un channel privé et invisible. Chaque problème sera donc automatiquement connu par tout les membres. Inutile de spam un staff., inline = False)
-            embed.add_field(name = "\u200B", value = "*11** Toute sorte de pub ou promotion est interdite sans l'accord de la modération.", inline = False)
-            embed.add_field(name = "\u200B", value = "*11** puie ici si tu les as lu. Merci!", inline = False)            embed.set_thumbnail(url = bot.user.avatar_url)
-            embed.set_author(name = "Among Us France", icon_url= bot.user.avatar_url)
+            embed.add_field(name = "\u200B", value = "Sachez que le staff communique via un channel privé et invisible. Chaque problème sera donc automatiquement connu par tout les membres. Inutile de spam un staff.", inline = False)
+            embed.add_field(name = "\u200B", value = "**11.** Toute sorte de pub ou promotion est interdite sans l'accord de la modération.", inline = False)
+            embed.add_field(name = "\u200B", value = "👇 Appuie ici si tu les as lu. Merci!", inline = False)
+            embed.set_thumbnail(url = self.bot.user.avatar_url)
+            embed.set_author(name = "Among Us France", icon_url= self.bot.user.avatar_url)
             await ctx.send(embed = embed)
             
 def setup(bot):
