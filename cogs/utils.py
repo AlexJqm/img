1b3d890d487445a75a9f675e91e6d52b61e699e0
Glitch (nonstop-exclusive-amaranthus) <none>
2020-10-19 18:52:56 +0000

    🍪☺️ Checkpoint ./cogs/utils.py:22204162/958 ./start.sh:22204162/2 ./requirements.txt:22204162/9

--- cogs/utils.py
+++ cogs/utils.py
@@ -8,6 +8,7 @@ from database.connect import db_connect
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+import requests
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -164,22 +165,31 @@ class Utils(commands.Cog):
             
             
     @commands.command(pass_context=True)
-    async def test(ctx, user: discord.Member):
+    async def test(self, ctx, member: discord.Member = None):
         img = Image.open("infoimgimg.png") #Replace infoimgimg.png with your background image.
         draw = ImageDraw.Draw(img)
         font = ImageFont.truetype("Modern_Sans_Light.otf", 100) #Make sure you insert a valid font from your folder.
         fontbig = ImageFont.truetype("Fitamint Script.ttf", 400) #Make sure you insert a valid font from your folder.
+        
+        with requests.get(member.avatar_url) as r:
+            img_data = r.content
+        with open('profile.jpg', 'wb') as handler:
+            handler.write(img_data)
+        im2 = Image.open("profile.jpg")
+        
+        
         #    (x,y)::↓ ↓ ↓ (text)::↓ ↓     (r,g,b)::↓ ↓ ↓
+        print(member.name)
         draw.text((200, 0), "Information:", (255, 255, 255), font=fontbig) #draws Information
-        draw.text((50, 500), "Username: {}".format(user.name), (255, 255, 255), font=font) #draws the Username of the user
-        draw.text((50, 700), "ID:  {}".format(user.id), (255, 255, 255), font=font) #draws the user ID
-        draw.text((50, 900), "User Status:{}".format(user.status), (255, 255, 255), font=font) #draws the user status
-        draw.text((50, 1100), "Account created: {}".format(user.created_at), (255, 255, 255), font=font) #When the account was created 
-        draw.text((50, 1300), "Nickname:{}".format(user.display_name), (255, 255, 255), font=font) # Nickname of the user
-        draw.text((50, 1500), "Users' Top Role:{}".format(user.top_role), (255, 255, 255), font=font) #draws the top rome
-        draw.text((50, 1700), "User Joined:{}".format(user.joined_at), (255, 255, 255), font=font) #draws info about when the user joined
+        draw.text((50, 500), "Username: {}".format(member.name), (255, 255, 255), font=font) #draws the Username of the user
+        draw.text((50, 700), "ID:  {}".format(member.id), (255, 255, 255), font=font) #draws the user ID
+        draw.text((50, 900), "User Status:{}".format(member.status), (255, 255, 255), font=font) #draws the user status
+        draw.text((50, 1100), "Account created: {}".format(member.created_at), (255, 255, 255), font=font) #When the account was created 
+        draw.text((50, 1300), "Nickname:{}".format(member.display_name), (255, 255, 255), font=font) # Nickname of the user
+        draw.text((50, 1500), "Users' Top Role:{}".format(member.top_role), (255, 255, 255), font=font) #draws the top rome
+        draw.text((50, 1700), "User Joined:{}".format(member.joined_at), (255, 255, 255), font=font) #draws info about when the user joined
         img.save('infoimg2.png') #Change infoimg2.png if needed.
-        await self.bot.upload("infoimg2.png")
+        await ctx.send('Hello', file=discord.File('infoimg2.png', 'profile.jpg'))
 
 def setup(bot):
     bot.add_cog(Utils(bot))
