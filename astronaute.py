c526b0222cb83c30180ab766ee067d451783ed16
Glitch (nonstop-exclusive-amaranthus) <none>
2020-10-24 17:50:22 +0000

    🏄👫 Checkpoint ./astronaute.py:22204162/995 ./cogs/utils.py:22204162/4255

--- astronaute.py
+++ astronaute.py
@@ -4,10 +4,7 @@ from discord.ext import commands
 from colorama import Fore, init
 intents = discord.Intents.default()
 intents.members = True
-intents.typing = False
-intents.presences = False
-
-bot = commands.Bot(command_prefix='.')
+bot = commands.Bot(command_prefix = '.', intents = intents)
 bot.remove_command('help')
 
 for file in os.listdir("cogs"):
@@ -16,6 +13,13 @@ for file in os.listdir("cogs"):
         bot.load_extension(f"cogs.{name}")
 
 print(Fore.RED + "Bot launched" + Fore.RESET)
+
+@bot.command(pass_context = True)
+async def stat(ctx):
+    print(bot.get_all_members().count())
+    for member in bot.get_all_members():
+        if member.status != discord.Status.offline:
+            print(member.name, member.status)
 
 if __name__ == "__main__":
     bot.run("NzU2NTE2MDUyNTE0MzczNzM0.X2S-eg.5thR1HK5BJ6OPrFLoTasEFLijJk")
