bee9d13895ab042cd19e433b3f23b493c79ba197
Glitch (nonstop-exclusive-amaranthus) <none>
2020-10-26 02:50:04 +0000

    ☎️🌍 Checkpoint ./cogs/honor.py:22204162/2083

--- cogs/honor.py
+++ cogs/honor.py
@@ -40,18 +40,33 @@ class Honor(commands.Cog):
         
         else: await ctx.send(embed = discord.Embed(title = "💥 Une erreur s'est produite...", description = "Vous n'avez pas mentionner de joueur: `.like @pseudo`", color = 0xF73F26))
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
     @commands.command(pass_context = True, aliases=['dl'])
     async def dislike(self, ctx, member: discord.Member = None):
         await ctx.channel.purge(limit = 1)
+        if member is not None:
+            if member.id != ctx.message.author.id:
+                
+                data = users.find({'player_id': member.id})
+                user = {}
+                for i in data: user = i
+                if user == {}:
+                    db_user = User(
+                        player_id = member.id,
+                        like = 0,
+                        dislike = 0,
+                        badge = []
+                    )
+                    json_data = json.loads(db_user.to_json())
+                    result = users.insert_one(json_data)
+                    users.update_one({'player_id': member.id}, {"$set": {'dislike': user['dislike'] + 1}})
+                    await ctx.send(embed = discord.Embed(title = f"💔 Dislike", description = f"{ctx.message.author.mention}, votre dislike a bien été pris en compte!", color = 0x26f752))
+                else:
+                    users.update_one({'player_id': member.id}, {"$set": {'dislike': user['dislike'] + 1}})
+                    await ctx.send(embed = discord.Embed(title = f"💔 Dislike", description = f"{ctx.message.author.mention}, votre dislike a bien été pris en compte!", color = 0x26f752))
+                    
+            else: await ctx.send(embed = discord.Embed(title = "💥 Une erreur s'est produite...", description = "Vous ne pouvez pas vous dislikez vous-même.", color = 0xF73F26))  
+        
+        else: await ctx.send(embed = discord.Embed(title = "💥 Une erreur s'est produite...", description = "Vous n'avez pas mentionner de joueur: `.like @pseudo`", color = 0xF73F26))
         
 def setup(bot):
     bot.add_cog(Honor(bot))
