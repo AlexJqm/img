6241e2d1ddef527749d9a286c21ad4f9c1eed196
Glitch (nonstop-exclusive-amaranthus) <none>
2020-10-28 02:18:30 +0000

    🐯🏢 Checkpoint ./events/voice_update.py:22204162/1166

--- events/voice_update.py
+++ events/voice_update.py
@@ -1,18 +1,28 @@
- import discord
-import asyncio
+import discord
 from discord.ext import commands, tasks
-import time
-import datetime
-import pytz
+from database.connect import db_connect
+
+servers = db_connect('servers')
 
 class Utils(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
-    @commands.command(pass_context = True)
-    async def report(self, ctx):
-        
-        data = servers.find({})
-      
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        print(member.name)
+        try: print("before :", before.channel.name)
+        except: pass
+        try: print("after :", after.channel.name)
+        except: pass
+        data = servers.find({"current_players": {"$in": [member.name]}})
+        player = {}
+        for i in data: player = i
+        voice = discord.utils.get(member.guild.channels, name = player['voice_name'].capitalize())
+        print(voice.name)
+
+        if voice.name != before.channel.name:
+            print('hello')
+
 def setup(bot):
     bot.add_cog(Utils(bot))
